c047ee6c76a608ab343c47eaa2b2652af0369a70
David Storer <dstorer2@gmail.com>
2022-02-15 12:28:02 -0500

    new word

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [cellContent, setCellContent] = useState([])
   const [activeRow, setActiveRow] = useState(0);
   const [previousGuesses, setPreviousGuesses] = useState([]);
-  const solution = "FROWN";
+  const solution = "HIRED";
 
   // const apiUrl = process.env.API_URL || "http://localhost:";
   // const apiPort = process.env.REACT_APP_API_PORT || 1234;
